872f4cd0394afeb7c39e75323008b16c67dcee6f
ViSHWA <154061854+ViSHWA14510@users.noreply.github.com>
2024-10-28 12:29:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 API_ID = int(os.environ.get("API_ID", "27344784"))
 API_HASH = os.environ.get("API_HASH", "a12613f54c74d00bfb76cf8f0688c2ee")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6804187518:AAGMOF30ZP-qQxfw5vKKxOXDnn4EsO6Pb3I")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7650081375:AAFzBJO7rxVD5pNMDglUEPDI_rSk5i-abO0")
 DB_CHANNEL_ID = os.environ.get("DB_CHANNEL_ID", "-1002056408106")
 IS_PRIVATE = os.environ.get("IS_PRIVATE" ,"True") # any input is ok But True preferable
 OWNER_ID = int(os.environ.get("OWNER_ID", "6899191648"))
